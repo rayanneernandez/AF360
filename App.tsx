e65c569d3b42d3f6953c0120411a3667078cc1cd
Rayanne Ernandez <rayanne.ernandez@globaltera.com.br>
2026-09-03 13:05:49 -0300

    App: aumenta espacamento entre titulo e subtitulo no cabecalho azul de Entrar como...

--- App.tsx
+++ App.tsx
@@ -17664,7 +17664,7 @@ export const styles = StyleSheet.create({
     fontWeight: '800',
   },
   selectPanelHero: {
-    paddingBottom: 20,
+    paddingBottom: 30,
     paddingHorizontal: 24,
     borderBottomLeftRadius: 28,
     borderBottomRightRadius: 28,
@@ -17673,7 +17673,7 @@ export const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    marginBottom: 8,
+    marginBottom: 16,
   },
   selectPanelHeroIconShell: {
     width: 36,
